d30129390e04b9d9b02782bb3c05fa3c01fd313f
Youssef Albert <joeedward394@gmail.com>
2025-05-18 12:23:14 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:elevate_task/shoes_home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductScreen(),
+    );
+  }
+}
